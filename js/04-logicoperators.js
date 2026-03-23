c2bcbf95e377115bfdbe93d706ace5d95b3cee97
Daniel <puricchdaniil@gmail.com>
2026-03-23 22:11:36 +0200

    Refactor variables to use const for immutability in logic operators examples

--- js/04-logicoperators.js
+++ js/04-logicoperators.js
@@ -1,6 +1,6 @@
 // 1. Перевірка заповнення двох текстових полів
-let field1 = "Hello";
-let field2 = "World";
+const field1 = "Hello";
+const field2 = "World";
 if (field1 !== "" && field2 !== "") {
     console.log("Обидва поля заповнені");
 } else {
@@ -8,9 +8,9 @@ if (field1 !== "" && field2 !== "") {
 }
 
 // 2. Сума двох чисел
-let num1 = 7;
-let num2 = 5;
-let sum = num1 + num2;
+const num1 = 7;
+const num2 = 5;
+const sum = num1 + num2;
 if (sum > 10) {
     console.log("Сума більша за 10");
 } else {
@@ -18,15 +18,15 @@ if (sum > 10) {
 }
 
 // 3. Перевірка наявності слова "JavaScript" у тексті
-let text = "Я вивчаю JavaScript!";
-if (text.includes("JavaScript")) {
+const text = "Я вивчаю JavaScript!";
+if (text.toLowerCase().includes("javascript")) {
     console.log("Текст містить слово JavaScript");
 } else {
     console.log("Текст не містить слово JavaScript");
 }
 
 // 4. Перевірка, чи число в діапазоні від 10 до 20
-let number = 15;
+const number = 15;
 if (number > 10 && number < 20) {
     console.log("Число входить в діапазон від 10 до 20");
 } else {
@@ -34,14 +34,14 @@ if (number > 10 && number < 20) {
 }
 
 // 5. Перевірка полів ім'я, email, пароль
-let name = "Dan";
-let email = "dan@example.com";
-let password = "secret1";
-let isNameValid = name.length >= 3;
-let atIndex = email.indexOf("@");
-let dotIndex = email.indexOf(".", atIndex);
-let isEmailValid = atIndex > 0 && dotIndex > atIndex + 1;
-let isPasswordValid = password.length >= 6;
+const name = "Dan";
+const email = "dan@example.com";
+const password = "secret1";
+const isNameValid = name.length >= 3;
+const atIndex = email.indexOf("@");
+const dotIndex = email.indexOf(".", atIndex);
+const isEmailValid = atIndex > 0 && dotIndex > atIndex + 1;
+const isPasswordValid = password.length >= 6;
 if (isNameValid && isEmailValid && isPasswordValid) {
     console.log("Перенаправлення на іншу сторінку");
 } else {
