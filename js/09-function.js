54fbb3680ad619389c36fa1f92864559296ab850
Daniel <puricchdaniil@gmail.com>
2026-04-11 19:09:54 +0300

    Add calculateEngravingPrice function to 09-function.js for price calculation

--- js/09-function.js
+++ js/09-function.js
@@ -7,3 +7,12 @@ const logItems = function (array) {
 
 // Перевірка
 // logItems(['Mango', 'Poly', 'Ajax']);
+
+// 2.
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const words = message.split(' ');
+  return words.length * pricePerWord;
+};
+
+// Перевірка
+// console.log(calculateEngravingPrice('Пробне повідомлення для гравіювання', 10));
