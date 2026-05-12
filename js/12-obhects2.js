39c647da1cecaf4173357474f892c1a799ced216
Daniel <puricchdaniil@gmail.com>
2026-05-12 17:25:52 +0300

    Refactor functions in 12-obhects2.js to use const for function declarations and remove unnecessary comments

--- js/12-obhects2.js
+++ js/12-obhects2.js
@@ -1,5 +1,4 @@
 // 1
-
 const user = {
     name: "Mango",
     age: 20,
@@ -11,26 +10,24 @@ user.mood = "happy";
 user.hobby = "skydiving";
 user.premium = false;
 
-for (const key of Object.keys(user)) {
+const keys = Object.keys(user);
+for (const key of keys) {
     console.log(`${key}: ${user[key]}`);
 }
 
 
 // 2
-
-function countProps(obj) {
+const countProps = function(obj) {
     return Object.keys(obj).length;
-}
+};
 
-// check result
 console.log(countProps({})); 
 console.log(countProps({ name: "Mango", age: 2 })); 
 console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); 
 
 
 // 3
-
-function findBestEmployee(employees) {
+const findBestEmployee = function(employees) {
     let bestEmployee = "";
     let maxTasks = 0;
 
@@ -42,30 +39,14 @@ function findBestEmployee(employees) {
     }
 
     return bestEmployee;
-}
+};
 
-// check result
-console.log(
-    findBestEmployee({
-        ann: 29,
-        david: 35,
-        helen: 1,
-        lorence: 99,
-    })
-);
-
-console.log(
-    findBestEmployee({
-        poly: 12,
-        mango: 17,
-        ajax: 4,
-    })
-);
+console.log(findBestEmployee({ ann: 29, david: 35, helen: 1, lorence: 99 }));
+console.log(findBestEmployee({ poly: 12, mango: 17, ajax: 4 }));
 
 
 // 4
-
-function countTotalSalary(employees) {
+const countTotalSalary = function(employees) {
     let totalSalary = 0;
 
     for (const salary of Object.values(employees)) {
@@ -73,23 +54,20 @@ function countTotalSalary(employees) {
     }
 
     return totalSalary;
-}
+};
 
-// check result
 console.log(countTotalSalary({}));
-
-console.log(
-    countTotalSalary({
-        mango: 100,
-        poly: 150,
-        alfred: 80,
-    })
-);
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
 
 
 // 5
+const products = [
+    { name: "Apple", price: 30, quantity: 3 },
+    { name: "Lemon", price: 20, quantity: 5 },
+    { name: "Peach", price: 50, quantity: 2 },
+];
 
-function getAllPropValues(arr, prop) {
+const getAllPropValues = function(arr, prop) {
     const values = [];
 
     for (const item of arr) {
@@ -99,14 +77,7 @@ function getAllPropValues(arr, prop) {
     }
 
     return values;
-}
-
-// check result
-const products = [
-    { name: "Apple", price: 30, quantity: 3 },
-    { name: "Lemon", price: 20, quantity: 5 },
-    { name: "Peach", price: 50, quantity: 2 },
-];
+};
 
 console.log(getAllPropValues(products, "name"));
 console.log(getAllPropValues(products, "price"));
@@ -114,8 +85,7 @@ console.log(getAllPropValues(products, "category"));
 
 
 // 6
-
-function calculateTotalPrice(allProducts, productName) {
+const calculateTotalPrice = function(allProducts, productName) {
     for (const product of allProducts) {
         if (product.name === productName) {
             return product.price * product.quantity;
@@ -123,16 +93,9 @@ function calculateTotalPrice(allProducts, productName) {
     }
 
     return 0;
-}
+};
 
-// check result
-const allProducts = [
-    { name: "Apple", price: 30, quantity: 3 },
-    { name: "Lemon", price: 20, quantity: 5 },
-    { name: "Peach", price: 50, quantity: 2 },
-];
-
-console.log(calculateTotalPrice(allProducts, "Apple"));
-console.log(calculateTotalPrice(allProducts, "Lemon"));
-console.log(calculateTotalPrice(allProducts, "Peach"));
-console.log(calculateTotalPrice(allProducts, "Banana"));
+console.log(calculateTotalPrice(products, "Apple"));
+console.log(calculateTotalPrice(products, "Lemon"));
+console.log(calculateTotalPrice(products, "Peach"));
+console.log(calculateTotalPrice(products, "Banana"));
